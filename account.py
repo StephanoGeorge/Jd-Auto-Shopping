8212ead56031c065eea5d7ece6ecb99993f91b13
nodisturb <nodisturb@nodisturb.com>
2020-02-11 21:35:03 +0800

    更新 requestUntilSuccess 的参数

--- account.py
+++ account.py
@@ -20,9 +20,7 @@ class Account:
             '检测登录', globals.GET,
             'https://passport.jd.com/loginservice.aspx?method=Login',
             headers={'Referer': 'https://www.jd.com/'},
-            sess=self.sess,
-            sleepTime=0,
-            attemptTimes=5).json()['Identity']['IsAuthenticated']
+            sess=self.sess).json()['Identity']['IsAuthenticated']
 
     def buy(self, itemId):
         if self.isBuying:
@@ -33,20 +31,13 @@ class Account:
             if globals.requestUntilSuccess(
                     '添加到购物车', globals.GET, 'https://cart.jd.com/gate.action',
                     params={'pid': itemId, 'pcount': int(random() * 5) + 1, 'ptype': 1},
-                    sess=self.sess,
-                    logLvl=logging.ERROR,
-                    timeout=3,
-                    sleepTime=0.5,
-                    attemptTimes=3) is None:
+                    sess=self.sess, logLvl=logging.ERROR, timeout=3) is None:
                 return
 
             # 结算
             resp = globals.requestUntilSuccess(
                 '结算', globals.GET, 'https://trade.jd.com/shopping/order/getOrderInfo.action',
-                sess=self.sess,
-                timeout=3,
-                sleepTime=0.5,
-                attemptTimes=3)
+                sess=self.sess, timeout=3)
             if resp is None:
                 return
             self.config['riskControl'] = re.search('riskControl" value="(.+?)"', resp.text).group(1)
@@ -64,19 +55,17 @@ class Account:
                         'submitOrderParam.ignorePriceChange': '0',
                         'submitOrderParam.btSupport': '0',
                         # https://gia.jd.com/fcf.html
-                        'submitOrderParam.eid': self.config['eid'],
-                        'submitOrderParam.fp': self.config['fp'],
+                        'submitOrderParam.eid': self.config['eid'],  # 几乎不变
+                        'submitOrderParam.fp': self.config['fp'],  # 几乎不变
                         'riskControl': self.config['riskControl'],
                         'submitOrderParam.jxj': 1,
                         'submitOrderParam.trackId': self.config['trackId'],
                         'submitOrderParam.isBestCoupon': 1,  #
                         'submitOrderParam.needCheck': 1,  #
                     },
-                    checkFun=lambda _resp: _resp.json()['resultCode'] in (60123, 600157, 600158),
+                    checkFun=lambda _resp: _resp.json()['success'],
                     logLvl=logging.ERROR,
-                    timeout=3,
-                    sleepTime=0.5,
-                    attemptTimes=3) is None:
+                    timeout=3) is None:
                 return
         finally:
             self.isBuying = False

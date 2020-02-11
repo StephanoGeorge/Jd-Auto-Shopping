1acb7772365847fcac4803cdf65d03461de2c8a9
nodisturb <nodisturb@nodisturb.com>
2020-02-11 11:52:45 +0800

    修改注释为: 检查登录

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import monitor
 # 设置日志
 logging.getLogger('urllib3').setLevel(logging.WARNING)
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
-# 保持登录
+# 检查登录
 monitor.checkLogin()
 # 监控
 monitor.monitor()

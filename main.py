9a289d2f38b02766db1a1544e39113e148cd7c81
nodisturb <nodisturb@nodisturb.com>
2020-02-13 21:17:10 +0800

    移除 basicConfig 的注释

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import monitor
 # 设置日志
 logging.getLogger('urllib3').setLevel(logging.FATAL)
 logging.getLogger().setLevel(logging.FATAL)
-# logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
 # 检查登录
 Thread(target=monitor.checkLogin).start()
 # 监控

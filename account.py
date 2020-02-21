359fd245ef3772a192a5605360f26cc15aaa45e3
Ian Beach <nodisturb@nodisturb.com>
2020-02-21 11:27:38 +0800

    为提交订单失败增加 debug

--- account.py
+++ account.py
@@ -8,36 +8,35 @@ import requests
 
 
 class Account:
-    def __init__(self, id, _config):
+    def __init__(self, id, config):
         self.id = id
-        self.config = _config['config']
+        self.config = config['config']
         self.sess = requests.Session()
-        self.sess.cookies.update(_config['cookies'])
+        self.sess.cookies.update(config['cookies'])
         self.sess.headers.update(glb.reqHeaders)
         self.isBuying = False
 
     def checkLogin(self):
         return glb.request(
-            '检测登录', glb.GET, 'https://passport.jd.com/loginservice.aspx?method=Login',
+            '检测登录', self.sess, glb.GET, 'https://passport.jd.com/loginservice.aspx?method=Login',
             headers={'Referer': 'https://www.jd.com/'},
-            sess=self.sess, logLvl={glb.successLogLvl: logging.DEBUG}, timeout=5)
+            logLvl={glb.successLogLvl: logging.DEBUG,
+                    glb.timeoutLogLvl: logging.DEBUG}, timeout=5)
 
     def buy(self, itemId):
         while self.isBuying:
-            if not glb.items[itemId]:
+            if not glb.canBuy(itemId):
                 return
         self.isBuying = True
         success = [False]
         logging.warning('开始购买 ({})'.format(', '.join((self.id, itemId))))
         try:
-            # TODO: 查看是否限购
             # 添加到购物车
             if glb.request(
-                    '添加到购物车 ({})'.format(', '.join((self.id, itemId))), glb.GET, 'https://cart.jd.com/gate.action',
-                    params={'pid': itemId, 'pcount': 1, 'ptype': 1},
-                    sess=self.sess, redirect=False,
-                    logLvl={glb.defaultLogLvl: logging.ERROR,
-                            glb.redirectLogLvl: logging.DEBUG}, timeout=3) is None:
+                    '添加到购物车 ({})'.format(', '.join((self.id, itemId))), self.sess, glb.GET,
+                    'https://cart.jd.com/gate.action', params={'pid': itemId, 'pcount': 1, 'ptype': 1},
+                    redirect=False, logLvl={glb.defaultLogLvl: logging.ERROR,
+                                            glb.redirectLogLvl: logging.DEBUG}, timeout=3) is None:
                 return
             logging.warning('添加到购物车 ({}) 成功'.format(', '.join((self.id, itemId))))
 
@@ -50,9 +49,9 @@ class Account:
 
             # 结算
             resp = glb.request(
-                '结算 ({})'.format(', '.join((self.id, itemId))), glb.GET,
+                '结算 ({})'.format(', '.join((self.id, itemId))), self.sess, glb.GET,
                 'https://trade.jd.com/shopping/order/getOrderInfo.action',
-                sess=self.sess, checkFuc=getOrderInfoCheck, args=(self,),
+                checkFuc=getOrderInfoCheck, args=(self,),
                 redirect=False, logLvl={glb.defaultLogLvl: logging.ERROR}, timeout=3)
             if resp is None:
                 return
@@ -75,12 +74,13 @@ class Account:
                 else:
                     logging.error('提交订单 ({}) 失败 ({})'.format(
                         ', '.join((args[0].id, itemId)),
+                        resp.json()['resultCode'],
                         _resp.json()))
                     return False
 
             # 提交订单
             if glb.request(
-                    '提交订单 ({})'.format(', '.join((self.id, itemId))), glb.POST,
+                    '提交订单 ({})'.format(', '.join((self.id, itemId))), self.sess, glb.POST,
                     'https://trade.jd.com/shopping/order/submitOrder.action',
                     headers={'Origin': 'https://trade.jd.com',
                              'Referer': 'https://trade.jd.com/shopping/order/getOrderInfo.action'},
@@ -106,13 +106,14 @@ class Account:
         finally:
             # 失败后删除商品
             if not success[0]:
-                glb.request('从购物车删除 ({})'.format(', '.join((self.id, itemId))), glb.POST,
-                            'https://cart.jd.com/removeSkuFromCart.action',
-                            data={'venderId': '8888', 'pid': itemId, 'ptype': '1', 'packId': '0',
-                                  'targetId': '0', 't': '0', 'outSkus': '', 'random': '0.3794799431176733',
-                                  'locationId': self.config['areaId']},
-                            headers={'Content-Type': 'application/x-www-form-urlencoded',
-                                     'Origin': 'https://cart.jd.com',
-                                     'Referer': 'https://cart.jd.com/cart.action'},
-                            sess=self.sess, timeout=5)
+                glb.request(
+                    '从购物车删除 ({})'.format(', '.join((self.id, itemId))), self.sess, glb.POST,
+                    'https://cart.jd.com/removeSkuFromCart.action',
+                    data={'venderId': '8888', 'pid': itemId, 'ptype': '1', 'packId': '0',
+                          'targetId': '0', 't': '0', 'outSkus': '', 'random': '0.3794799431176733',
+                          'locationId': self.config['areaId']},
+                    headers={'Content-Type': 'application/x-www-form-urlencoded',
+                             'Origin': 'https://cart.jd.com',
+                             'Referer': 'https://cart.jd.com/cart.action'},
+                    timeout=5)
             self.isBuying = False

81e9080900a34e02c1b445e8d89a77f5d774c9fa
Ian Beach <nodisturb@nodisturb.com>
2020-02-21 08:27:58 +0800

    add checkSnappingUp(), if resp is not None

--- monitor.py
+++ monitor.py
@@ -39,6 +39,22 @@ def monitor():
     logging.info('开始监控库存')
     for isInStockApiParam in isInStockApiParams:
         Thread(target=_monitor, args=(isInStockApiParam,)).start()
+    Thread(target=checkSnappingUp).start()
+
+
+def checkSnappingUp():
+    for itemId in glb.config['items'].keys():
+        resp = glb.request('检查是否为抢购商品', glb.GET, 'https://yushou.jd.com/youshouinfo.action',
+                           params={'sku': itemId},
+                           headers={'referer': 'https://item.jd.com/{}.html'.format(itemId),
+                                    'cookie': None},
+                           logLvl={glb.successLogLvl: logging.DEBUG,  # 请求成功的日志等级
+                                   glb.timeoutLogLvl: logging.DEBUG,  # 请求超时的日志等级
+                                   glb.tooManyFailureLogLvl: logging.DEBUG},  # 过多失败的日志等级
+                           timeout=5)
+        if resp is not None and resp.text != '{"error":"pss info is null"}':
+            glb.items[itemId]['snappingUp'] = True
+        time.sleep(10)
 
 
 def _monitor(isInStockApiParam):
@@ -49,18 +65,17 @@ def _monitor(isInStockApiParam):
                                    glb.timeoutLogLvl: logging.DEBUG,  # 请求超时的日志等级
                                    glb.tooManyFailureLogLvl: logging.DEBUG},  # 过多失败的日志等级
                            timeout=1.5)
-        if resp is None:
-            continue
-        try:
-            for itemId, value in resp.json().items():
-                if value['skuState'] == 0 or value['StockState'] not in (33, 40):
-                    glb.items[itemId] = False
-                else:
-                    logging.warning('{} 有货'.format(itemId))
-                    glb.items[itemId] = True
-                    buy(itemId)
-        except JSONDecodeError:
-            continue
+        if resp is not None:
+            try:
+                for itemId, value in resp.json().items():
+                    if value['skuState'] == 0 or value['StockState'] not in (33, 40):
+                        glb.items[itemId]['inStock'] = False
+                    else:
+                        logging.warning('{} 有货'.format(itemId))
+                        glb.items[itemId]['inStock'] = True
+                        buy(itemId)
+            except JSONDecodeError:
+                continue
 
 
 def buy(itemId):

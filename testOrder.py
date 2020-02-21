015eba649eea463d6e61b0060f97ad51c5cbfa62
Ian Beach <nodisturb@nodisturb.com>
2020-02-21 18:05:19 +0800

    change example

--- testOrder.py
+++ testOrder.py
@@ -1,5 +1,5 @@
 import glb
 
-id = 0  # 账户在 config.json 中的序号
-itemId = '3007224'  # 商品 ID
-glb.accountList[id].buy(itemId)
+id = 'yue'  # 账户在 config.json 中的 key
+itemId = '3007224'  # 有货的商品 ID
+glb.accountDict[id].buy(itemId)
